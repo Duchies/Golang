930f1475fa60ab87f846048d3779703ddd9564d4
Aman kumar <40682420+Duchies@users.noreply.github.com>
2024-12-24 18:22:32 +0530

    wait group use and goroutines

---
+++ waitGoupsAndgoroutines.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Planning a school trip")
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Rahul has done its task")
+		wg.Done()
+	}()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("tafe has done its task")
+		wg.Done()
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		fmt.Println("ziyo has done its task")
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println("all task done")
+}
